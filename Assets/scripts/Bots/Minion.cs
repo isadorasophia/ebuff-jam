37aa0e2c30343fa2cf72b36f7efeaaa0ede1b22f
Isadora Sophia <isaecia@gmail.com>
2016-06-04 05:20:02 -0300

    Added collision detection! :D

--- Assets/scripts/Bots/Minion.cs
+++ Assets/scripts/Bots/Minion.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Minion : MonoBehaviour {
+public class Minion : MonoBehaviour
+{
 
     enum Mode { Neutral, Cloud, Drink };
     enum Team { Neutral, Blue, Orange };
@@ -10,59 +11,69 @@ public class Minion : MonoBehaviour {
     private Mode c_mode;
     private Team c_team;
 
-    public float d_speed      = 0.0f; /* default speed */
+    public float d_speed = 0.0f; /* default speed */
 
-    private float c_speed     = 0.0f; /* current speed */
-    private float c_size      = 0.0f; /* current size */
+    private float c_speed = 0.0f; /* current speed */
+    private float c_size = 0.0f; /* current size */
 
     /* Custom configurations */
-    public float l_speed      = 5.0f;
-    public float h_speed      = 15.0f;
+    public float l_speed = 5.0f;
+    public float h_speed = 15.0f;
 
-    public float max_size     = 2;
-    public float min_size     = .5f;
+    public float max_size = 2;
+    public float min_size = .5f;
 
-    public int min_dis        = 100;
+    public int min_dis = 100;
 
     /* Gameplay settings */
     public Transform target;
 
     // use this for initialization
-    void Start() {
+    void Start()
+    {
         changeMode(Mode.Neutral);
         changeTeam(Team.Neutral);
     }
 
     // update is called once per frame
-    void Update() {
-        
+    void Update()
+    {
+
     }
 
     // called in fixed time, fancy!
     void FixedUpdate()
     {
-        /* Verify distances, without diagonals */
-        float xdist = Mathf.Abs(transform.position.x - target.position.x);
-        float ydist = Mathf.Abs(transform.position.y - target.position.y);
+        /* verify range */
+        float range = Vector2.Distance(transform.position, target.position);
 
-        if (xdist > min_dis || ydist > min_dis)
+        if (range > min_dis)
         {
-            if (xdist > ydist)
-            {
-                var c_dir = Mathf.Sign(target.position.x - transform.position.x);
+            /* Check colision! */
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, min_dis);
 
-                // move in x
-                transform.position = Vector2.right * c_dir * d_speed * Time.deltaTime;
-            } else
+            if (hit.transform != null && hit.transform.tag == "Obstacle")
             {
-                var c_dir = Mathf.Sign(target.position.y - transform.position.y);
-
-                // move in y
-                transform.position = Vector2.right * c_dir * d_speed * Time.deltaTime;
+                Vector3 dir = target.position - hit.transform.position;
+                
+                if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+                {
+                    transform.Translate(Mathf.Sign(dir.y) * Vector2.up  * d_speed * .5f * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(Mathf.Sign(dir.x) * Vector2.right * d_speed * .5f * Time.deltaTime);
+                }
             }
-        } else
+            else
+            {
+                /* Get the playe! */
+                transform.position = Vector2.MoveTowards(transform.position, target.position, d_speed * Time.deltaTime);
+            }
+        }
+        else
         {
-            // attack
+            // work on the attack
         }
 
         #region CameraLimits
@@ -71,7 +82,8 @@ public class Minion : MonoBehaviour {
         var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
         var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
         var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
-        // Keep in mind that we must consider the bottom as the ground, which is corrected by .12
+
+        // keep in mind that we must consider the bottom as the ground, which is corrected by .12
         var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, .12f, dist)).y;
 
         // now, checks if the player desired position has passed the limits - if so, force him to stay
@@ -110,15 +122,15 @@ public class Minion : MonoBehaviour {
 
         if (c_team == Team.Blue)
         {
-            
+
         }
         else if (c_team == Team.Orange)
         {
-            
+
         }
         else if (c_team == Team.Neutral)
         {
-            
+
         }
     }
 }
