3c038e8d33dcf84e393987b57a03328612376ee9
Isadora Sophia <isaecia@gmail.com>
2016-06-05 21:00:00 -0300

    changes to scripts.

--- Assets/Scripts/Controls/RandomMap.cs
+++ Assets/Scripts/Controls/RandomMap.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 
 public class RandomMap : MonoBehaviour {
     public int t_obj;
+    public int minions;
+
     public List<GameObject> obs;
+    public GameObject minion;
 
 	// Use this for initialization
 	void Start () {
@@ -23,15 +26,48 @@ public class RandomMap : MonoBehaviour {
             // Get camera offsets
             var dist = (transform.position - Camera.main.transform.position).z;
 
-            var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
-            var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
-            var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
-            var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+            var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(.1f, .1f, dist)).x;
+            var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .1f, dist)).x;
+            var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, .9f, dist)).y;
+            var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(.1f, .1f, dist)).y;
 
             x = Random.Range(leftBorder, rightBorder);
             y = Random.Range(bottomBorder, topBorder);
 
             Instantiate(obs[next], new Vector3(x, y, 0), obs[next].transform.rotation);
         }
-	}
+
+        /* generate the plantas */
+        for (int i = 0; i < t_obj/2; i++)
+        {
+            // Get camera offsets
+            var dist = (transform.position - Camera.main.transform.position).z;
+
+            var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(.1f, .1f, dist)).x;
+            var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .1f, dist)).x;
+            var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, .9f, dist)).y;
+            var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(.1f, .1f, dist)).y;
+
+            x = Random.Range(leftBorder, rightBorder);
+            y = Random.Range(bottomBorder, topBorder);
+
+            Instantiate(obs[0], new Vector3(x, y, 0), obs[0].transform.rotation);
+        }
+
+        for (int i = 0; i < minions; i++)
+        {
+            // Get camera offsets
+            var dist = (transform.position - Camera.main.transform.position).z;
+
+            var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(.1f, .1f, dist)).x;
+            var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(.9f, .1f, dist)).x;
+            var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, .9f, dist)).y;
+            var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, .1f, dist)).y;
+
+            x = Random.Range(leftBorder, rightBorder);
+            y = Random.Range(bottomBorder, topBorder);
+
+            Instantiate(minion, new Vector3(x, y, 0), minion.transform.rotation);
+        }
+    }
 }
