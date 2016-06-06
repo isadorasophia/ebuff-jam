66e4add7b7441295a73fb05ebf0d2a6a506ebda5
MrStrings <phstring95@hotmail.com>
2016-06-05 22:07:19 -0300

    Supa-alpha-build

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -4,15 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+	public bool autoRun = false;
+	public string toLoad = "Map";
+
 	// Use this for initialization
 	void Start () {
-	
+		if (autoRun)
+			Invoke ("Load", 3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.anyKeyDown) {
-			SceneManager.LoadScene ("Map");
+			SceneManager.LoadScene (toLoad);
 		}
+	}
+
+
+	void Load() {
+		SceneManager.LoadScene (toLoad);
 	}
 }
